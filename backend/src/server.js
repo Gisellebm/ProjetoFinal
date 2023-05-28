470402f51bcbc4b00aaf7ce3762fa0452c2a63e4
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-05-27 23:20:21 -0300

    Configurações e server.js

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.post("/users", (request, response) => {
+    const { name, email, password } = request.body;
+    response.json({ name, email, password });
+})
+
+const PORT = 3333
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
